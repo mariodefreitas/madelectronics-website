132edd4e11fdf6e1347e967f564f51cbeaddc150
Mario De Freitas <mariodefreitas6@live.com>
2026-03-19 10:30:25 +0000

    Update

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -24,8 +24,8 @@ const Navbar = () => {
                                 <Image
                                 src="/images/MAD.jpg"
                                 alt="Logo Image"
-                                width={200}
-                                height={200}
+                                width={100}
+                                height={100}
                                 />
                             </span>
                         </Link>
