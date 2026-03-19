971341efc68b4c2199cb1ee4ca7ca113aece757b
Mario De Freitas <mariodefreitas6@live.com>
2026-03-19 23:11:36 +0000

    fhj

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -15,19 +15,19 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className="bg-white shadow-sm fixed w-full z-50 py-3">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <nav className="bg-white shadow-sm fixed w-full z-50">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3">
                 <div className="flex justify-between h-20">
                     <div className="flex items-center">
                         <Link href="/" className="flex-shrink-0">
-                            <div>
+                            <span>
                                 <Image
                                 src="/images/MAD Electronics.jpg"
                                 alt="Logo Image"
                                 width={160}
                                 height={160}
                                 />
-                            </div>
+                            </span>
                         </Link>
                     </div>
 
