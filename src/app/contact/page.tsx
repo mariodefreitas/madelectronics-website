c520bd3fdc2cbe035d84daa3279f99ee2fbe4b6e
Mario De Freitas <mariodefreitas6@live.com>
2026-03-21 00:07:08 +0000

    jhgug

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -173,15 +173,5 @@ export default function ContactPage() {
                 </div>
             </div>
         </div>
-        <script type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
-</script>
-<script type="text/javascript">
-   (function(){
-      emailjs.init({
-        publicKey: "Puy0bUNBW9HbYulI4",
-      });
-   })();
-</script>
     );
 }
