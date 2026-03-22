135186836427c2b2d105ea650a4c4d47054d41fa
Mario De Freitas <mariodefreitas6@live.com>
2026-03-22 11:19:01 +0000

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Cpu, Users, Heart } from "lucide-react";
+//import { Cpu, Users, Heart } from "lucide-react";
 
 export default function Home() {
   const offers = [
