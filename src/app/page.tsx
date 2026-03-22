12a814a1d6c0b248c01e4be4ecc6107f4c9af8a5
Mario De Freitas <mariodefreitas6@live.com>
2026-03-22 11:16:33 +0000

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,10 +64,11 @@ export default function Home() {
         </div>
       </section>
 
-      {/* About Section 
+      {/* About Section */}
           <section className="bg-gradient-to-r from-orange-400 via-orange-500 to-red-700  text-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
+          {/*
             <h2 className="text-3xl font-bold text-white mb-4">
               Features
             </h2>
