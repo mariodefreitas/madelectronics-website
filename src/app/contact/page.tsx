de1635030f186fcf27d38170890f82bf1a48eb44
Mario De Freitas <mariodefreitas6@live.com>
2026-03-23 11:40:56 +0000

    Update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,29 +4,25 @@ import { useState } from 'react';
 export default function MySelect() {
   const [selected, setSelected] = useState("");
 
-  const handleChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-        const { name, value } = e.target;
-        setSelected((prev) => ({ ...prev, [name]: value }));
-    };
-
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+  };
 
   return (
     <div>
-		<label htmlFor="options">Choose your device: </label>
-		<select 
-			id="options" 
-			value={formData.device} 
-			onChange={handleChange}
-			className="border p-2 rounded"
-		>
-			<option value="" disabled>Select one...</option>
-			<option value="option1">Laptop</option>
-			<option value="option2">Smart Phone</option>
-			<option value="option3">Tablet</option>
-			<option value="option3">Game Console</option>
-			<option value="option3">Other</option>
+      <label htmlFor="options">Choose an option: </label>
+      <select 
+        id="options" 
+        value={selected} 
+        onChange={handleChange}
+        className="border p-2 rounded"
+      >
+		<option value="" disabled>Select one...</option>
+		<option value="option1">Laptop</option>
+		<option value="option2">Smart Phone</option>
+		<option value="option3">Tablet</option>
+		<option value="option3">Game Console</option>
+		<option value="option3">Other</option>
 	  </select>
 	  <p>Selected: {selected}</p>
 	</div>
