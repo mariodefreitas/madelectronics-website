2199e0b2853216ef95f7d3c569f72e8fa76c2b33
Mario De Freitas <mariodefreitas6@live.com>
2026-03-23 09:10:49 +0000

    Update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -142,19 +142,19 @@ export default function ContactPage() {
                                         />
                                     </div>
                                     <div>
-                                      <label htmlFor="options">Choose your device: </label>
-                                      <select 
-                                        id="options" 
-                                        value={formData.device} 
-                                        onChange={handleChange}
-                                        className="border p-2 rounded"
-                                      >
-                                        <option value="" disabled>Select one...</option>
-                                        <option value="option1">Laptop</option>
-                                        <option value="option2">Smart Phone</option>
-                                        <option value="option3">Tablet</option>
-                                        <option value="option3">Game Console</option>
-                                        <option value="option3">Other</option>
+                                        <label htmlFor="options">Choose your device: </label>
+                                        <select 
+                                            id="options" 
+                                            value={formData.device} 
+                                            onChange={handleChange}
+                                            className="border p-2 rounded"
+                                        >
+                                            <option value="" disabled>Select one...</option>
+                                            <option value="option1">Laptop</option>
+                                            <option value="option2">Smart Phone</option>
+                                            <option value="option3">Tablet</option>
+                                            <option value="option3">Game Console</option>
+                                            <option value="option3">Other</option>
                                       </select>
                                       <p>Selected: {selected}</p>
                                     </div>
