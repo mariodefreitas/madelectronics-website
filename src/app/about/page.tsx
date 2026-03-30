ec9ecb02d250089ef14620bce78ae298aa912995
Mario De Freitas <mariodefreitas6@live.com>
2026-03-31 00:24:59 +0100

    Update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -62,11 +62,13 @@ export default function AboutPage() {
                 <div className="grid md:grid-cols-2 gap-12 items-center">
                     <div>
                         <h2 className="text-3xl font-bold mb-6 text-gray-800">Our Story</h2>
-                        <h3 className="text-3xl font-bold mb-6 text-gray-800">Where it all began</h3>
+                        <h1 className="text-3xl font-bold mb-6 text-gray-800">Where it all Began</h1>
                         <p className="text-gray-600 mb-4">
-                            An electronics engineering students bedroom back in 2013. In this picture my first workbench on a old computer stand with the Mrs PacMan main motherboard on the bench can be seen on the right hand side with some testing equipment.
+                            An electronics engineering students bedroom back in 2013. In this picture my first workbench on a old computer stand with a 1980 motherboard on the bench can be seen on the right hand side with some electronics testing equipment.
+                            <p className="text-gray-600">
                             Then amoungst all the electronic devices I tested, repaired and leant from the most important one. Has got to be the 1980s Mrs Pacman machine. As a child I spent many hours playing it and I believe somewhere deep
                             down in my childhood memories that machine sparked a love for electronics and a desire to understand how it all work. It was the catalyst that set me on the path to becoming an electronics engineer and eventually founding MAD Electronics.
+                            </p>
                         </p>
                     </div>
                     <div className="relative h-96">
